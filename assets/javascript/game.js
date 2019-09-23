e729cc3581ed52a1b9a515249664a39a8d903bb7
slenksp <slenksp@github.com>
2019-09-22 22:37:43 -0500

    added html and js files

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("body").css("background-color", "#51d0de");
+  $("#rules").css("display", "none");
+  $("#rules").css("position", "relative");
+  $("#showRules").click(function(e) {
+    e.preventDefault();
+    $("#rules").toggle(1000);
+    //   $(test).toggle();
+    // $("#rules").css("z-index", 1);
+    $("rules").css("z-index", 1);
+    $("rules").css("top", 0);
+  });
+});
